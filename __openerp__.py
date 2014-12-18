0048932445ae3e1a776cf3994eba68eaa644dc83
aliomattux <kyle.waid@gcotech.com>
2014-12-18 16:15:06 -0600

    Add dependency on JIT mdule

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
     'version': '1.1',
     'author': 'Kyle Waid',
     'category': 'Sales Management',
-    'depends': ['stock'],
+    'depends': ['stock', 'procurement_jit'],
     'website': 'https://www.gcotech.com',
     'description': """ 
     """,
